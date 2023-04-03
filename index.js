bb21ead49ffdc27432cac6671926558cabf2ab21
vCesar1mx <cesarvaz.e7@gmail.com>
2023-04-03 16:36:40 -0600

    Add - GET route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
-
+const path = require('path');
 const app = express();
 
 // Configuración de Morgan y Compression
@@ -10,11 +10,27 @@ app.use(compression());
 
 // Rutas de la aplicación
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+app.get('/api/payment/:process', (req, res) => {
+    
+    switch (process) {
+        case 'create':
+            
+            break;
+        case 'retrieve':
+            
+            break;
+        case 'capture':
+            
+            break;
+        default:
+            break;
+    }
 });
 
 // Iniciar servidor HTTP
-const PORT = 3000;
+const PORT = 25997;
 app.listen(PORT, () => {
     console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
